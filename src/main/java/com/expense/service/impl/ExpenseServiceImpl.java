36e9635b50c749b8ef18e26179480230f7bc52f7
samidhagoyal <107755840+samidhagoyal@users.noreply.github.com>
2022-06-22 11:36:31 +0000

    Update ExpenseServiceImpl.java

--- src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
+++ src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
@@ -54,7 +54,7 @@ public class ExpenseServiceImpl implements ExpenseService {
 	@Override
 	public void deleteById(Long expenseId) {
 		//Buggy Code(ExpenseControllerTest.testDeleteExpense())
-		log.info("Debug delete");
+		//log.info("Debug delete");
 		
 		//Correct Code
 		expenseRepository.deleteById(expenseId);
